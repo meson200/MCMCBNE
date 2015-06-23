5b8edd314b8ef5a5303679d7785ae213d3313a9a
Sangkyu Lee <meson200@gmail.com>
2015-06-23 16:55:06 -0400

    updated a data import module

---
+++ kyu_BN_readdata.m
@@ -0,0 +1,193 @@
+function [data,data_c,data_missing,data_c_missing,labels,mi,studyid,FracSize,COMSI] = kyu_BN_readdata(SBRTfilter,disc)
+
+% reads patient data from 2 .xls files (one clinical/dosimetric, one biological) into a data matrix 
+% data matrices are in the following dimensions:
+% # rows: number of the variables selected at the input dialog + 1 (outcome)
+% # cols: number of patients
+% the last row is reserved for an outcome vector
+%
+% inputs
+%
+% SBRTfilter = 2: include only conventioanl fx (fraction size < 2)
+% SBRTfilter = 3: include only SBRT patients (fraction size > 3)
+% SBRTfilter = 1: SBRT+Conventional
+% 
+% disc = 2: supervised (maximum mutual information)
+% disc = 3: unsupervised (k-means)
+%
+% outputs
+%
+% data: discretized data/ missing data filled in
+% data_c: data in a z-scored continuous scale/ missing data filled in 
+% data_missing: discretized data/ missing data left as NaN
+% data_c_missing: continuous cata / missing data left as NaN
+% labels: a cell array of variable names
+% mi: mutual information between variables and RP
+% studyid: cell array of patient identifiers. one letter for an institution followed by a
+% 3-digit number (ex:W001)
+% FracSize: fraction size of the imported patients
+% COMSI: superior-inferior location of a PTV centroid 
+%
+% normalization option
+% norm = 0: don't normalize, data in raw scale
+% norm = 1: standardization (z-score) 
+% norm = 2: rescaling (min-max) 
+% norm = 3: standardization + hyper tangent mapping 
+norm = 1;
+% number of bins for discretization 
+bins = 2;
+% imputation method
+imp = 2;
+% imp = 1: fill the missing values with the group median 
+% imp = 2: k-nearest neighbor (fill with the values of the most similar
+% entries)
+class_name = 'RP'; % choose your endpoint here. will be saved to an array "class"
+
+% path to the xls files
+% physical and biological data separated in two .xls files
+dir_name = '~/Box Sync/SKyu/lungdata/';
+filename_bio = 'biomarkers_20150622.xls';
+filename_phy = 'dosimetry_clinical_20150622.xls';
+fullpath_bio = cat(2,dir_name,filename_bio);
+fullpath_phy = cat(2,dir_name,filename_phy);
+% read the WashU data first and obtain the list of variables from the user prompt 
+[data_raw_phy,data_raw_phy_missing,class,pts_to_include,studyid,FracSize] = kyu_readphysical(fullpath_phy,imp,class_name);
+[data_raw_bio,data_raw_bio_missing] = kyu_readbiomarkers(fullpath_bio,studyid,FracSize,imp);
+data_raw = [data_raw_bio data_raw_phy];
+data_raw_missing = [data_raw_bio_missing data_raw_phy_missing];
+for i = 1:numel(data_raw_phy)
+   if strcmp(data_raw_phy(1,i).name,'PTVCOMSI') 
+       COMSI = data_raw_phy(1,i).value(pts_to_include);
+   end
+end
+
+% input dialog
+disp('The following variables are found: \n');
+for k = 1:size(data_raw,2)
+    disp(sprintf('%d. %s \n',k,data_raw(k).name{1,1}));
+end
+resp = input('choose the variables, separated by comma: ','s');
+if ~isempty(resp)
+    temp = textscan(resp,'%d','delimiter',',');
+    vars = cell2mat(temp);
+else
+    vars = 1:size(data_raw,2);  
+end
+labels = {};
+num_nodes = numel(vars);
+for i=1:num_nodes
+    ind = vars(i);
+    labels{i} = data_raw(ind).name{1,1};
+end 
+data_X_c_raw = kyu_preprocess(data_raw,labels,pts_to_include,[],[]);
+data_X_c_raw_missing = kyu_preprocess(data_raw_missing,labels,pts_to_include,[],[]);
+studyid = studyid(pts_to_include);
+FracSize = FracSize(pts_to_include);
+COMSI = COMSI(pts_to_include);
+
+% apply a fraction size filter
+switch SBRTfilter
+    case 1
+       FracFilter = find(FracSize>0);
+    case 2
+       FracFilter = find(FracSize<=2);
+    case 3
+       FracFilter = find(FracSize>=3);
+end
+data_X_c_raw = data_X_c_raw(FracFilter,:);
+data_X_c_raw_missing = data_X_c_raw_missing(FracFilter,:);
+class = class(FracFilter);
+FracSize = FracSize(FracFilter);
+COMSI = COMSI(FracFilter);
+studyid = studyid(FracFilter);
+
+% normalize 
+[data_X_c,data_X_c_missing] = kyu_normalize(data_X_c_raw,data_X_c_raw_missing,norm);
+
+% discretize
+[data_X,data_X_missing,b_filled,b_missing,mi] = kyu_discretize(data_X_c,data_X_c_missing,class,disc,bins);
+
+% plot discretization results
+%scales_plot = [3700/10^6,1,1,1,1/1000,1,1/1000,1,1,1/100,1/100,1,100,100,1/365,1];
+%mp2013_plotbins(data_X_c,data_X_c_raw,b_filled,labels,scales_plot);
+
+
+% eliminate missing data (for variable selection)
+data_X_forKS = data_X;
+class_forKS = class;
+rowtodelete = [];
+for i = 1:size(data_X,1)
+   if ~isempty(find(isnan(data_X_missing(i,:)))) 
+      rowtodelete = [rowtodelete i]; 
+   end
+end
+data_X_forKS(rowtodelete,:) = [];
+class_forKS(rowtodelete) = [];
+%mi = p;
+
+%optional: Sparse PCA
+% for j = 5:num_nodes
+%     [all_cards,all_vars,all_Zs] = sparsePCA(data_X_c,j,j,10,1);
+%     var_selected = find(all_Zs);  
+%     varstr = [];
+%     for k = var_selected'
+%         varstr = strcat(varstr,',',labels{k});
+%     end
+%     disp(['variables selected: ',varstr,' variance:',num2str(all_vars)]);
+% end
+
+events = find(class==2);
+
+% add a class to a data
+data = reshapeforbn(data_X,class);
+data_c = reshapeforbn(data_X_c,class);
+data_missing = reshapeforbn(data_X_missing,class);
+data_c_missing = reshapeforbn(data_X_c_missing,class);
+
+disp('--------------odds ratio of the selected variables-----------------');
+y_trn = class-1;
+oddsratio = zeros(3,size(data_X,2));
+for i=1:num_nodes
+    x_trn = data_X_c_missing(:,i);
+    % remove NaN
+    keeprows = ~isnan(x_trn) & ~isnan(y_trn);
+    x_trn_2 = x_trn(keeprows);
+    y_trn_2 = y_trn(keeprows);
+    [~,b_trn,se_trn,~,~] = drxlr_logistic_regression(x_trn_2,y_trn_2,300,1e-4);
+    or_trn = [b_trn(1),b_trn(1)-1.96*se_trn(1),b_trn(1)+1.96*se_trn(1)];
+    or_trn = exp(or_trn(1,:));
+    oddsratio(:,i) = or_trn';
+    str = sprintf('%d.%s : %4.2f(%4.2f,%4.2f)',i,labels{i},or_trn(1),or_trn(2),or_trn(3));
+    disp(str)      
+end
+
+% save the imported data into the .csv file
+content = [data_X_c_missing'; class'];
+content2 = num2cell(content);
+header = labels';
+header = cat(1,header,'RP');
+DmatF = [header content2]; 
+for i = 1:size(DmatF,1)
+    for j = 1:size(DmatF,2)
+        if isnan(DmatF{i,j})
+           DmatF{i,j} = ''; 
+        end
+    end
+end
+cell2csv('ForMIS.csv',DmatF');
+
+function data_out = reshapeforbn(data_in,class)
+    
+data_out = [data_in class];
+data_out = data_out';
+
+
+    
+
+
+
+
+
+
+
+
--- kyu_readphysical.m
+++ kyu_readphysical.m
@@ -1,10 +1,11 @@
-function [data_raw,data_raw_missing,class,pts_to_include,studyid,FracSize] = kyu_readphysical(NameoftheFile,imp)
+function [data_raw,data_raw_missing,class,pts_to_include,studyid,FracSize] = kyu_readphysical(NameoftheFile,imp,class_name)
 
 % read the .xls file that contains physical/clinical variables
 % input
 %
 % NameoftheFile: path to the .xls file
 % imp: imputation method
+% class_name: name of the variable that should be a class
 %
 % output
 %
@@ -12,9 +13,9 @@ function [data_raw,data_raw_missing,class,pts_to_include,studyid,FracSize] = kyu
 % data_raw_missing: same as data_raw, except that missing entries left NaN
 % class: a vector of an outcome (RP in this case)
 % pts_to_include: patients with value=1 is included into the training data
+% studyid: cell array of patient identifiers. one letter for an institution followed by a
+% 3-digit number (ex:W001)
 
-
-studyid = [];
 data_raw = struct('name',{},'value',{});
 [num,txt] = xlsread(NameoftheFile);
 dim = size(txt);
@@ -30,18 +31,38 @@ FracSize = ones(samplesize,1);
 % goes through column names found in the .xls file
 % instructions to enumerate data depends on the type of variables
 i=1;
+
+% offset is needed for indexing numeric columns
+% if the first column is string
+instit = cell(samplesize,1);
+offset = 1;
 for k=1:size(txt,2)
     header = char(txt(1,k));
     switch header
-        case 'Seq'
-            studyid = num(:,k);
+        case 'Instit' % institution. under construction
+            instit = txt(2:end,k);
+        case 'Seq' % patient ID
+            ptno = num(:,k-offset);
         case 'Rx'    
         case 'DOB' 
         case 'XRTStart'
         case 'RPdate'
         case 'RP_coded'    
         case 'RP_coded2'
-        case 'failure'   
+        case 'failure'
+            tmparray = txt(2:end,k);
+            tmparray2 = zeros(samplesize,1);
+            data_raw(i).name = txt(1,k);
+            
+            for u = 1:samplesize
+               if strcmp('no disease',tmparray(u))
+                   tmparray2(u) = 1;
+               else
+                   tmparray2(u) = 2;  
+               end
+            end    
+            data_raw(i).value = tmparray2;
+            i = i+1;
         case 'RPdate'    
         case 'LastFU'     
         case 'FUmonths'
@@ -55,7 +76,7 @@ for k=1:size(txt,2)
         % RP    
         case 'RP'
             for u=1:samplesize
-                if num(u,k) < 2 
+                if num(u,k-offset) < 2 
                     tmparray(u) = 1; % define the event as RP grade >2 
                 else
                     tmparray(u) = 2;
@@ -63,26 +84,26 @@ for k=1:size(txt,2)
             end
             class = tmparray; % class in this case is a binary for RP occurrence
             data_raw(i).name = {'Pneumonitis'};
-            data_raw(i).value = num(:,k);
+            data_raw(i).value = num(:,k-offset);
             i = i+1;
         case 'age'
-           tmparray = num(:,k);
+           tmparray = num(:,k-offset);
            data_raw(i).name = {'age'};
            data_raw(i).value = tmparray;
            i = i+1;
         case 'include'    
-            pts_to_include = num(:,k);
+            pts_to_include = num(:,k-offset);
         case 'FracSize'
             data_raw(i).name = txt(1,k);
-            data_raw(i).value = num(:,k);
+            data_raw(i).value = num(:,k-offset);
             i = i+1;            
             for u=1:samplesize
                 %if num(u,k) ~= 2 
                 %    SBRT(u) = 2; 
                 %end
-                FracSize(u) = num(u,k);
+                FracSize(u) = num(u,k-offset);
             end  
-        case {'chemo','tumorstage','failure'}
+        case {'chemo','tumorstage'}
             %data_raw(i).name = txt(1,k);
             %data_raw(i).value = txt(2:end,k);
             %i = i+1;
@@ -93,11 +114,11 @@ for k=1:size(txt,2)
             for u = 1:samplesize
                switch tmparray{u}
                    case 'O'
-                       tmparray2(u) = 1;
+                       tmparray2(u) = 2;
                    case 'ex' % currently,ex-smokers are treated as non-smokers
-                       tmparray2(u) = 0;
+                       tmparray2(u) = 1;
                    case 'no'
-                       tmparray2(u) = 0;
+                       tmparray2(u) = 1;
                    otherwise
                        tmparray2(u) = NaN;
                end
@@ -108,14 +129,23 @@ for k=1:size(txt,2)
         case {'rpdate','fibdate'}    
         otherwise    
             data_raw(i).name = txt(1,k);
-            data_raw(i).value = num(:,k);
+            data_raw(i).value = num(:,k-offset);
             i = i+1;
     end
 end
 
-% add a random variable
-%data_raw(i).name = 'Random';
-%data_raw(i).value = round(1+2*rand(samplesize,1));
+% create instance idenfifier (source institution+ID number)
+studyid = cell(samplesize,1);
+for i = 1:samplesize
+    if ptno(i) < 10
+        studyid(i) = strcat(instit(i),'00',num2str(ptno(i)));
+    elseif ptno(i) < 100
+        studyid(i) = strcat(instit(i),'0',num2str(ptno(i)));
+    else
+        studyid(i) = strcat(instit(i),num2str(ptno(i)));
+    end
+end
+
 
 temp_missing = [];
 for i=1:length(data_raw)
@@ -137,6 +167,10 @@ data_raw_missing = data_raw;
 for i=1:length(data_raw)
    data_raw(i).value = temp(:,i);
    data_raw_missing(i).value = temp_missing(:,i); 
+   % identify a class variable
+   if strcmp(data_raw(i).name,class_name)
+       class = data_raw(i).value;
+   end
 end
 
 pts_to_include = find(pts_to_include==1);
